aaae76758a1e483c3da0fe1d4ed65fe80c738382
Larissa de Paula <larissadepaulagoncalves@gmail.com>
2023-06-28 11:26:40 -0300

    inicio da função extractLinks e o primeiro teste unitario

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-module.exports = () => {
-  // ...
-};
+function extractLinks(markdownContent) {
+
+  const linkRegex = /\[([^\]]+)\]\(([^)]+)\)/g;
+  const links = [];
+  let match;
+  while ((match = linkRegex.exec(markdownContent)) !== null) {
+    const text = match[1];
+    const href = match[2];
+    const link = `[${text}](${href})`;
+    links.push(link);
+  }
+
+  return links;
+}
+
+module.exports = extractLinks;
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,29 @@
-const mdLinks = require('../');
+const { describe, it, expect } = require('@jest/globals');
+const extractLinks = require('../src/index.js');
 
+describe('extractLinks', () => {
+  it('Deveria extrair os links, com text e href', () => {
+    const markdownContent = `
+      # Título
+      [link de exemplo](https://www.exemplo.com)
+      Aqui está outro modelo [modelo](https://www.modelo.com) em um parágrafo.
 
-describe('mdLinks', () => {
+      Uma comunidade open source nos propôs criar uma ferramenta, usando [Node.js](https://nodejs.org/)
 
-  it('should...', () => {
-    console.log('FIX ME!');
+      [Node.js](https://nodejs.org/pt-br/) é um ambiente de execução para JavaScript construído com o 
+      [motor de JavaScript V8 do Chrome](https://developers.google.com/v8/). Ele vai nos permitir executar o JavaScript no nosso sistema operacional
+    `;
+
+    const expectedLinks = [
+      '[link de exemplo](https://www.exemplo.com)',
+      '[modelo](https://www.modelo.com)',
+      '[Node.js](https://nodejs.org/)',
+      '[Node.js](https://nodejs.org/pt-br/)',
+      '[motor de JavaScript V8 do Chrome](https://developers.google.com/v8/)',
+    ];
+
+    const links = extractLinks(markdownContent);
+    console.log(links);
+    expect(links).toEqual(expectedLinks);
   });
-
 });
