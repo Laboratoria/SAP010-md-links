7dc1133f987c42f5c6c0389caf26621b2a16e974
Raquel Maia <raquelmaia860@gmail.com>
2023-07-08 10:58:47 -0300

    updatefuctions

--- mdlinks.js
+++ mdlinks.js
@@ -1,16 +1,54 @@
 #!/usr/bin/env node
+
 const program = require('commander');
-const mdlinks = require('./index.js');
+const path = require('path');
+const mdlinks = require('./src/lib/index.js');
+
 
 program
   .version('1.0.0')
   .arguments('<caminhoArquivo>')
   .option('--validate', 'Validar os links')
+  .option('--stats', 'Exibir estatísticas')
   .action((caminhoArquivo, options) => {
-    mdlinks(caminhoArquivo, options);
+    const arquivo = path.resolve(`src/pag/${caminhoArquivo}`);
+    mdlinks(arquivo, options)
+      .then(({ links, statistics }) => {
+        if (options.validate && options.stats) {
+          console.log(`Total: ${statistics.total}`);
+          console.log(`Unique: ${statistics.unique}`);
+          console.log(`Broken: ${statistics.broken}`);
+        } else if (options.stats) {
+          console.log(`Total: ${statistics.total}`);
+          console.log(`Unique: ${statistics.unique}`);
+        } else if (options.validate) {
+          links.forEach((link) => {
+            console.log(`href: ${link.href}`);
+            console.log(`text: ${link.text}`);
+            console.log(`file: ${link.file}`);
+            console.log((`status: ${link.ok}`));
+            console.log('----------------------------');
+          });
+        } else {
+          links.forEach((link) => {
+            console.log(`Link: ${link.href}`);
+            console.log(`Text: ${link.text}`);
+            console.log(`File: ${link.file}`);
+            console.log('----------------------------');
+          });
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   });
 
 program.parse(process.argv);
+
+
+
+
+
 
 
 
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,29 @@
-const mdLinks = require('../');
+import mdlinks from '../src/lib/index.js';
+import path from 'path';
 
-
-describe('mdLinks', () => {
-
-  it('should...', () => {
-    console.log('FIX ME!');
+describe('mdlinks', () => {
+  it('Deve retornar os links de um arquivo Markdown', () => {
+    const filePath = 'src/pag/teste.md';
+    return mdlinks(filePath).then(links => {
+      expect(links[0]).toEqual(
+        { text: 'Markdown', href: 'https://pt.wikipedia.n', file: 'teste.md' },
+      );
+    });
   });
 
+  it('Deve retornar o arquivo "teste.md"', () => {
+    const filePath = 'src/pag/teste.md';
+    return mdlinks(filePath).then((links) => {
+      expect(links[0].file).toBe(path.basename(filePath));
+    });
+  });
+  
+  it('Deve retornar um array vazio para um arquivo que não seja Markdown', () => {
+    const filePath = 'src/pag/arquivo.txt';
+    return mdlinks(filePath).then(links => {
+      expect(links).toEqual([]);
+    });
+  });
 });
+
+
