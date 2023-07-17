a560d937414cfc1276392d77221b7ca94b654584
Jéssica Zanon <jessicatorreszanon@gmail.com>
2023-07-17 17:10:02 -0300

    leituraDoArquivoMD

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-module.exports = () => {
-  // ...
-};
+const fs = require('fs');
+
+const filePath = './README.md';
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  
+  console.log(data);
+});
