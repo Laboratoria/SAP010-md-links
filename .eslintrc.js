5278f80ef3dfe0a05576c0eeb0a14ae040fbd631
vitoria cabral <vitoria.oliveira@alu.ufc.br>
2023-07-17 17:58:19 -0300

    instalação de jest e eslint

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    //pode adicionar regras personalizadas ou modificar as regras existentes.
+  },
+};
