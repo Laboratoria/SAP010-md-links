b9401e1a9520f080cbaf3772dd70748b00a34507
Aline <128093293+Alinedev85@users.noreply.github.com>
2023-07-22 11:49:23 -0300

    Marco 1 + complemento do teste

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,22 +1,31 @@
-
 const { expect } = require('chai');
-const readFile = require('../src/index'); 
+const readFile = require('../src/index');
 
 describe('readFile', () => {
-  it('deve ler o arquivo e resolver com o seu conteúdo', (done) => {
-    const caminhoDoArquivo = './files/files.md';
+it('deve ler o arquivo e resolver com o seu conteúdo', (done) => {
+const caminhoDoArquivo = './files/files.md';
 
-    readFile(caminhoDoArquivo)
-      .then((data) => {
-        expect(data).to.be.a('string'); 
-       
-        done(); //concluido
-      })
-      .catch((error) => {
-        done(error); 
-      });
+readFile(caminhoDoArquivo)
+  .then((data) => {
+    expect(data).to.be.a('string'); 
+   
+    done(); //concluido
+  })
+  .catch((error) => {
+    done(error); 
   });
+});
 
-  it('deve rejeitar a promessa se o arquivo não existir', (done) => {
-    const caminhoDoArquivo = './caminho/inexistente.md'; // fake
+it('deve rejeitar a promessa se o arquivo não existir', (done) => {
+const caminhoDoArquivo = './caminho/inexistente.md'; // fake
 
+readFile(caminhoDoArquivo)
+  .then((data) => {
+    done(new Error('Deveria ter rejeitado a promessa'));
+  })
+  .catch((error) => {
+    expect(error).to.be.an('error'); 
+    done(); 
+  });
+});
+});
