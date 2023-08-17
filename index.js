07878dcf81ca7c4b80121d4a4b06d5a7bda3ca1f
amandascam03 <amandascam03@gmail.com>
2023-08-17 18:27:21 -0300

    função mdLinks criada e retornando rota absoluta dos markdowns

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
-module.exports = () => {
-  // ...
-};
+const path = require('path');
+const fs = require('fs');
+// .md, .mkd, .mdwn, .mdown, .mdtxt, .mdtext, .markdown, .text
+function mdLinks(rota) {
+  return new Promise((resolve, reject) => {
+    if ((fs.existsSync(rota)) && (path.extname(rota) === '.md' || '.mkd' || '.mdwn' || '.mdown' || '.mdtxt' || '.mdtext' || '.markdown' || '.text')) {
+      resolve(path.resolve(rota));
+    } else {
+      reject(new Error('este arquivo não é um markdown'));
+    }
+  })
+}
+
+mdLinks('README.md')
+  .then(() => {
+    console.log(path.resolve('README.md'))
+  })
+  .catch((error) => {
+    console.log(new Error(error))
+  })
+
+// console.log(mdLinks('README.md'));
+
+module.exports = mdLinks;
