3d9f81de16cff446aeb74e9e4cb65277bde7b285
amandascam03 <amandascam03@gmail.com>
2023-08-18 20:10:51 -0300

    arquivos estão sendo lidos

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
 const path = require('path');
 const fs = require('fs');
-// .md, .mkd, .mdwn, .mdown, .mdtxt, .mdtext, .markdown, .text
+const { readFile } = require('fs/promises');
+
+// markdowns: .md, .mkd, .mdwn, .mdown, .mdtxt, .mdtext, .markdown, .text
+
 function mdLinks(rota) {
   return new Promise((resolve, reject) => {
     if ((fs.existsSync(rota)) && (path.extname(rota) === '.md' || '.mkd' || '.mdwn' || '.mdown' || '.mdtxt' || '.mdtext' || '.markdown' || '.text')) {
       resolve(path.resolve(rota));
+        readFile(rota, 'utf8', (err, data) => {
+          if (err) throw err;
+          console.log(data);
+        })
     } else {
       reject(new Error('este arquivo não é um markdown'));
     }
   })
 }
 
-mdLinks('README.md')
+mdLinks('teste.text')
   .then(() => {
-    console.log(path.resolve('README.md'))
+    console.log(path.resolve('teste.text'));
+    readFile('teste.text', 'utf8')
+      .then((texto) => {
+        console.log({ texto });
+      })
+      .catch((error) => {
+        throw error;
+      })
+
   })
   .catch((error) => {
     console.log(new Error(error))
