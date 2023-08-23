cdeec0d313d2f703dbed86246adc432d2b8fb9c2
amandascam03 <amandascam03@gmail.com>
2023-08-23 18:35:27 -0300

    código refatorado e testes ok

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const path = require('path');
 const fsPromise = require('fs').promises;
 const fs = require('fs');
-// const { readFile } = require('fs/promises');
 
-// markdowns: .md, .mkd, .mdwn, .mdown, .mdtxt, .mdtext, .markdown, .text
+function readFileContent(rota) {
+  return fsPromise.readFile(rota, 'utf-8');
+}
 
 function mdLinks(rota) {
   return new Promise((resolve, reject) => {
@@ -18,7 +19,7 @@ function mdLinks(rota) {
       reject(new Error('O arquivo não é um arquivo markdown'));
     }
 
-    fsPromise.readFile(rota, 'utf-8')
+    readFileContent(rota)
       .then((file) => {
 
         if (file.trim() === '') {
@@ -53,38 +54,4 @@ function mdLinks(rota) {
   })
 }
 
-// function mdLinks(rota) {
-//   return new Promise((resolve, reject) => {
-//     if (fs.existsSync(rota)) {
-//       if (path.extname(rota) === '.md' || '.mkd' || '.mdwn' || '.mdown' || '.mdtxt' || '.mdtext' || '.markdown' || '.text') {
-//         readFile(rota, 'utf8')
-//           .then((file) => {
-//             const regex = /\[([^\[]+)\]\((.*)\)/gim;
-
-//             const filePath = path.resolve(rota);
-
-//             const myMatch = file.match(regex);
-
-//             let array = [];
-
-//             const singleMatch = /\[([^\[]+)\]\((.*)\)/;
-//             for (var i = 0; i < myMatch.length; i++) {
-//               var text = singleMatch.exec(myMatch[i])
-//               array.push({ href: text[2], texto: text[1], file: filePath })
-//               // resolve([ { href: text[2] }, { texto: text[1]}, { file: filePath}])
-//             }
-//             resolve(array);
-
-//           })
-//           .catch((error) => {
-//             console.error(error.message);
-//           })
-//         }
-//     } else {
-//       reject(new Error('Este arquivo não existe'));
-//     }
-//   })
-// }
-// console.log(mdLinks('README.md'));
-
-module.exports = mdLinks;
+module.exports = mdLinks, readFileContent;
