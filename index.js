cc0f5eb55d482c983b3793c1b25b15ed61734c8b
amandascam03 <amandascam03@gmail.com>
2023-08-23 16:15:34 -0300

    tratamento de erros da função mdlinks

--- index.js
+++ index.js
@@ -1,40 +1,90 @@
 const path = require('path');
+const fsPromise = require('fs').promises;
 const fs = require('fs');
-const { readFile } = require('fs/promises');
+// const { readFile } = require('fs/promises');
 
 // markdowns: .md, .mkd, .mdwn, .mdown, .mdtxt, .mdtext, .markdown, .text
 
 function mdLinks(rota) {
   return new Promise((resolve, reject) => {
-    if ((fs.existsSync(rota)) && (path.extname(rota) === '.md' || '.mkd' || '.mdwn' || '.mdown' || '.mdtxt' || '.mdtext' || '.markdown' || '.text')) {
-      readFile(rota, 'utf8')
-        .then((file) => {
-          const regex = /\[([^\[]+)\]\((.*)\)/gim;
-
-          const filePath = path.resolve(rota);
-
-          const myMatch = file.match(regex);
-
-          let array = [];
-
-          const singleMatch = /\[([^\[]+)\]\((.*)\)/;
-          for (var i = 0; i < myMatch.length; i++) {
-            var text = singleMatch.exec(myMatch[i])
-            array.push({ href: text[2], texto: text[1], file: filePath })
-            // resolve([ { href: text[2] }, { texto: text[1]}, { file: filePath}])
-          }
-          resolve(array);
-
-        })
-        .catch((error) => {
-          console.error(error.message);
-        })
-    } else {
-      reject(new Error('este arquivo não é um markdown'));
-
+    if (!fs.existsSync(rota)) {
+      reject(new Error('Este arquivo não existe'));
     }
+
+    const mdExtensions = ['.md', '.mkd', '.mdwn', '.mdown', '.mdtxt', '.mdtext', '.markdown', 'text'];
+    const fileExtension = path.extname(rota);
+
+    if (!mdExtensions.includes(fileExtension)) {
+      reject(new Error('O arquivo não é um arquivo markdown'));
+    }
+
+    fsPromise.readFile(rota, 'utf-8')
+      .then((file) => {
+
+        if (file.trim() === '') {
+          reject(new Error('O arquivo está vazio'));
+        }
+
+        const regex = /\[([^\[]+)\]\((.*)\)/gim;
+
+        const filePath = path.resolve(rota);
+
+        const myMatch = file.match(regex);
+
+        if (myMatch === null) {
+          resolve('Nenhum link encontrado');
+          return;
+        }
+
+        let array = [];
+
+        const singleMatch = /\[([^\[]+)\]\((.*)\)/;
+        for (var i = 0; i < myMatch.length; i++) {
+          var text = singleMatch.exec(myMatch[i])
+          array.push({ href: text[2], texto: text[1], file: filePath })
+          // resolve([ { href: text[2] }, { texto: text[1]}, { file: filePath}])
+        }
+        resolve(array);
+
+      })
+      .catch((error) => {
+        reject(error);
+      })
   })
 }
+
+// function mdLinks(rota) {
+//   return new Promise((resolve, reject) => {
+//     if (fs.existsSync(rota)) {
+//       if (path.extname(rota) === '.md' || '.mkd' || '.mdwn' || '.mdown' || '.mdtxt' || '.mdtext' || '.markdown' || '.text') {
+//         readFile(rota, 'utf8')
+//           .then((file) => {
+//             const regex = /\[([^\[]+)\]\((.*)\)/gim;
+
+//             const filePath = path.resolve(rota);
+
+//             const myMatch = file.match(regex);
+
+//             let array = [];
+
+//             const singleMatch = /\[([^\[]+)\]\((.*)\)/;
+//             for (var i = 0; i < myMatch.length; i++) {
+//               var text = singleMatch.exec(myMatch[i])
+//               array.push({ href: text[2], texto: text[1], file: filePath })
+//               // resolve([ { href: text[2] }, { texto: text[1]}, { file: filePath}])
+//             }
+//             resolve(array);
+
+//           })
+//           .catch((error) => {
+//             console.error(error.message);
+//           })
+//         }
+//     } else {
+//       reject(new Error('Este arquivo não existe'));
+//     }
+//   })
+// }
 // console.log(mdLinks('README.md'));
 
 module.exports = mdLinks;
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,4 +1,5 @@
 const mdLinks = require('../index');
+const readFile = require('../index');
 
 describe('teste da função mdLinks', () => {
   test('a função mdLinks resolve uma array de objetos', async () => {
@@ -9,7 +10,14 @@ describe('teste da função mdLinks', () => {
   test('a função rejeita o arquivo', async () => {
     const fakeMdFile = 'arquivo.txt';
     return mdLinks(fakeMdFile).catch(error => {
-      expect(error.message).toBe("este arquivo não é um markdown");
+      expect(error.message).toBe('Este arquivo não existe');
     });
   });
+
+  test('rejeita se o arquivo estiver vazio', async () => {
+    const fakeFile = '';
+    return readFile(fakeFile, 'utf-8').catch(error => {
+      expect(error.message).toBe('Este arquivo não existe');
+    });
+  })
 })
