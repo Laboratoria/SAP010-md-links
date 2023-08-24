d0ee486adfeb9f731b9a3f357fdf9189f1f24df9
amandascam03 <amandascam03@gmail.com>
2023-08-24 09:42:20 -0300

    cobertura de testes 100%

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -3,7 +3,7 @@ const mdLinks = require('../index');
 const readFileContent = require('../index');
 // const path = require('path');
 
-describe('teste da função mdLinks', () => {
+describe('testes da função mdLinks', () => {
   test('a função mdLinks deve resolver uma array de objetos', async () => {
     const funcaoMdLinks = await mdLinks('README.md');
     expect(typeof funcaoMdLinks).toBe('object');
@@ -14,14 +14,14 @@ describe('teste da função mdLinks', () => {
     const readFileContent = jest.fn();
     readFileContent(rota);
     expect(readFileContent).toHaveBeenCalledTimes(1);
-  })
+  });
 
   test('deve rejeitar se o arquivo estiver vazio', async () => {
     const fakeFile = 'arquivoinexist.md';
     return mdLinks(fakeFile).catch(error => {
       expect(error.message).toBe('Este arquivo não existe');
     });
-  })
+  });
 
   test('deve rejeitar o arquivo diferente de md', async () => {
     const fakeMdFile = 'index.js';
@@ -29,33 +29,13 @@ describe('teste da função mdLinks', () => {
       expect(error.message).toBe('O arquivo não é um arquivo markdown');
     });
   });
+})
 
-
-  // test('os links foram recebidos', () => {
-  //   const rota = 'teste.md';
-  //   return expect(mdLinks(rota)).resolves.toStrictEqual([
-  //     {
-  //       href: 'https://github.com/markedjs/marked',
-  //       texto: 'marked',
-  //       file: 'C:\\Users\\Janio\\SAP010-md-links\\teste.md'
-  //     },
-  //     {
-  //       href: 'https://github.com/workshopper/learnyounode',
-  //       texto: 'learnyounode',
-  //       file: 'C:\\Users\\Janio\\SAP010-md-links\\teste.md'
-  //     },
-  //     {
-  //       href: 'https://docs.npmjs.com/getting-started/what-is-npm',
-  //       texto: 'NPM ',
-  //       file: 'C:\\Users\\Janio\\SAP010-md-links\\teste.md'
-  //     }
-  //   ])
-  // })
-
-  // test('a função rejeita o arquivo', async () => {
-  //   const fakeMdFile = 'arquivo.txt';
-  //   return mdLinks(fakeMdFile).catch(error => {
-  //     expect(error.message).toBe('Este arquivo não existe');
-  //   });
-  // });
+describe('testes da função readFileContent', () => {
+  test('deve rejeitar o arquivo vazio', async () => {
+    const emptyFile = 'empty.md';
+    return readFileContent(emptyFile).catch(error => {
+      expect(error.message).toBe('O arquivo está vazio');
+    });
+  });
 })
